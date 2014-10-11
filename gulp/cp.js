29ab7d0e75bcd3258882ae8ad5f2c3d15f8547e6
Pascal Weiland <pascal@seehin.de>
2014-10-11 13:54:19 +0200

    Added angular and angular-route with bower

--- gulp/cp.js
+++ gulp/cp.js
@@ -1,10 +1,14 @@
 var gulp = require('gulp');
 var reload = require('browser-sync').reload;
 
-// copy vendors, fonts, static files (views, txt, icons)
 module.exports = function(){
 
-  gulp.src(['app/static/favicon.ico', 'app/static/robots.txt', 'app/static/humans.txt'])
-      .pipe(gulp.dest('./dist'))
-      .pipe(reload({stream:true}));
+  gulp
+    .src(['app/static/favicon.ico', 'app/static/robots.txt', 'app/static/humans.txt'])
+    .pipe(gulp.dest('./dist'))
+    .pipe(reload({stream:true}));
+
+  gulp
+    .src(['bower_components/angular/angular.js', 'bower_components/angular-route/angular-route.js'])
+    .pipe(gulp.dest('./dist/js'));
 };
